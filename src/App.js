929a69bcbb612de580794fcfc43497d011fc4ea8
Code <119617297+Vpan23@users.noreply.github.com>
2023-06-18 23:02:25 -0500

    Subida de hoja de estilos - proyecto-Testimonio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio'
 
 function App() {
   return (
     <div className="App">
+      <div className='contenedor-principal'>
+      <h1>Esto es lo que dicen nuestros alumnos sobre nosotros!</h1>
+      <Testimonio />
+      </div>
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import '../styles-sheets/Testimonio.css';
 
 function Testimonio() {
   return (
     <div className='contenedor-testimonio'>
-      <img className='imagen-testimonio' src={require('../imagenes/testimonio1.jpg')} alt='Foto de Rubius'/>
+      <img className='imagen-testimonio' src={require('../imagenes/testimonio1.jpeg')} alt='Foto de sana'width={"200px"}/>
       <div className='contenedor-texto-testimonio'>
-      <p className='nombre-testimonio'>Emma Bostian en Suecias</p>
-      <p className='cargo-testimonio'>Ingenieria de Software</p>
-      <p className='texto-testimonio'>Siempre he sido buena en mate</p>
+      <p className='nombre-testimonio'>Sana</p>
+      <p className='cargo-testimonio'>Kpop Idol : Twice</p>
+      <p className='texto-testimonio'>    Lorem ipsum dolor sit amet consectetur adipisicing elit. A nihil quod repudiandae nobis doloremque corporis voluptate facilis molestias! Quo, aspernatur illo. Dolorem sint velit deleniti fugiat natus debitis, laborum nisi! Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. A nihil quod repudiandae nobis doloremque corporis voluptate facilis molestias!</p>
       </div>
     </div>
   );  
 }
+
+export default Testimonio; 
